9797d7ea740811aee913dfc4c38c556aa155df2e
SwampMaster2160 <jaxon1454@gmail.com>
2023-10-09 12:51:16 +1300

    work on new tp

--- src/client/java/com/swampmaster2160/morecommandsforreindev/client/mixins/MixinPlayerCommandHandler.java
+++ src/client/java/com/swampmaster2160/morecommandsforreindev/client/mixins/MixinPlayerCommandHandler.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.swampmaster2160.morecommandsforreindev.commands.CommandKill;
 import com.swampmaster2160.morecommandsforreindev.commands.CommandListEntities;
+import com.swampmaster2160.morecommandsforreindev.commands.CommandNewTP;
 import com.swampmaster2160.morecommandsforreindev.commands.CommandSeed;
 import com.swampmaster2160.morecommandsforreindev.commands.CommandSetWorldSpawn;
 import com.swampmaster2160.morecommandsforreindev.commands.CommandSummon;
@@ -15,6 +16,7 @@ import com.swampmaster2160.morecommandsforreindev.commands.CommandWorldInfo;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.mitask.PlayerCommandHandler;
+import net.minecraft.mitask.command.Command;
 
 @Mixin(PlayerCommandHandler.class)
 public abstract class MixinPlayerCommandHandler {
@@ -28,5 +30,15 @@ public abstract class MixinPlayerCommandHandler {
 		((PlayerCommandHandler)(Object)this).addCommand(new CommandSummon(((PlayerCommandHandler)(Object)this)));
 		((PlayerCommandHandler)(Object)this).addCommand(new CommandKill(((PlayerCommandHandler)(Object)this)));
 		((PlayerCommandHandler)(Object)this).addCommand(new CommandListEntities(((PlayerCommandHandler)(Object)this)));
+		replaceCommand(new CommandNewTP(((PlayerCommandHandler)(Object)this)));
+	}
+
+	public void replaceCommand(Command command) {
+		for(Command oldCommand : PlayerCommandHandler.commands) {
+			if (oldCommand.getName() != command.getName()) continue;
+			PlayerCommandHandler.commands.remove(oldCommand);
+			break;
+		}
+		((PlayerCommandHandler)(Object)this).addCommand(command);
 	}
 }
